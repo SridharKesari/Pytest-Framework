c97b6b901a33ebc48b87334556bfb98f34cbf02b
SridharKesari <kesari.sridhar@gmail.com>
2024-07-24 00:56:01 +0530

    get api
    get api

--- APIPetstoreTests/test_basics.py
+++ APIPetstoreTests/test_basics.py
@@ -14,3 +14,14 @@ def test_getPetById_response():
     data = response.json()
     print(json.dumps(data, indent=4))  # print(data)
     assert len(data) > 0, 'Empty Response'
+
+
+# testing response body for ID key
+def test_getPetById_id():
+    url = baseURI + petID
+    header = {'Content-Type': 'application/json'}  # required during POST
+    print('RequestURL:', url)
+    response = requests.get(url, verify=False, headers=header)
+    data = response.json()
+    assert data['id'] == 151
+    assert data['name'] == 'doggie'
