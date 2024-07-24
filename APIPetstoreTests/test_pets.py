6533db7c0ec425df89ee1687e88e965d053b54c8
SridharKesari <kesari.sridhar@gmail.com>
2024-07-25 00:40:28 +0530

    added post lib

--- APIPetstoreTests/test_pets.py
+++ APIPetstoreTests/test_pets.py
@@ -1,9 +1,17 @@
-from utils.libutils import getAPIData
+from utils.libutils import getAPIData, postAPIData
 from utils.config_parser import getPetAPIURL
 
 # baseURI = 'https://petstore.swagger.io/v2/pet/'
 petID = '191'
 baseURI = getPetAPIURL()  # use config parser to get the URL from the pets_qa.ini config file
+
+
+def test_postPetByDetails_request():
+    data, resp_status, timeTaken = postAPIData(baseURI, petID, 'Cutie', 'available')
+    assert data['id'] == int(petID)
+    assert len(data) > 0
+    print(data)
+    print(f'Time Taken: {timeTaken}')
 
 
 def test_getPetById_response():
--- utils/libutils.py
+++ utils/libutils.py
@@ -11,3 +11,13 @@ def getAPIData(url):
     assert len(data) > 0, 'Empty Response'
     timeTaken = response.elapsed.total_seconds()
     return data, response.status_code, timeTaken
+
+
+def postAPIData(url, petID, name, status):
+    header = {'Content-Type': 'application/json'}
+    payload = {'id': petID, 'name': name, 'status': status}
+    response = requests.post(url, verify=False, json=payload, headers=header)
+    data = response.json()
+    assert len(data) > 0
+    timeTaken = response.elapsed.total_seconds()
+    return data, response.status_code, timeTaken
